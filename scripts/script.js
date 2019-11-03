3c4f460956eb518afba0a0881a7b7d591bc1789f
Dincius <55960253+Dincius@users.noreply.github.com>
2019-11-03 23:15:14 +0200

    Cleaned script

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,14 @@
-var total = 0;
-var average = 0;
 
 function adder() {
-  var sk1 = parseInt(document.getElementById("num1").value);
-  var sk2 = parseInt(document.getElementById("num2").value);
-  var sk3 = parseInt(document.getElementById("num3").value);
-  var sk4 = parseInt(document.getElementById("num4").value);
-  var sk5 = parseInt(document.getElementById("num5").value);
-  var sk6 = parseInt(document.getElementById("num6").value);
-  total = sk1 + sk2 + sk3 + sk4 + sk5 + sk6;
-  average = total / 6;
-  document.getElementById("p1").innerHTML = average;
+var summ = 0;
+var average = 0;
+for(var a = 0; a < 24; a++) {
+    summ += parseInt(document.getElementById(a + 1).value);
+}
+average = summ / 6;
+document.getElementById("summ").innerHTML = average;
+}
+
+function myFunction() {
+  document.getElementById("myform").reset();
 }
